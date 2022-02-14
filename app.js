d6dce5aa71d88493595e6e44c65f8ef84821c7ba
Thymmz <atharva.deshpande228@gmail.com>
2022-02-13 20:27:29 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+
+
+//middleware
+
+app.use(express.json())
+
+
+
+//routes
+
+app.get('/hello', (req, res)=>{
+   res.send('Task Manager App')
+   //console.log("Req", req);
+   //console.log("Res", res);
+})
+
+app.use('/api/v1/tasks', tasks)
+
+
+
+
+
+
+
+
+
+
+const port = 3000
+
+
+app.listen(port,console.log(`server is listening on port ${port} ...`))
