86e33ae666e7d3f5ab359e8f5ea3dfab797788e1
Thymmz <atharva.deshpande228@gmail.com>
2022-02-13 20:46:29 -0500

    initial setup

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,27 @@
 const getAllTasks = (req, res)=>{
-   res.send('all items')
+   res.send('get all tasks')
+}
+
+const createTask = (req, res) =>{
+   res.send('new task')
+}
+
+const getTask = (req, res) =>{
+   res.send('get task')
+}
+
+const updateTask = (req, res) =>{
+   res.send('update task')
+}
+
+const deleteTask = (req, res) =>{
+   res.send('delete tasks')
 }
 
 module.exports = {
    getAllTasks,
+   createTask,
+   getTask,
+   updateTask,
+   deleteTask,
 }
